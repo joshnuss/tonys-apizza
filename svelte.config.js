5394a628d6c1bc41babc9a9445b93b57c54f78eb
Joshua Nussbaum <joshnuss@gmail.com>
2022-06-07 23:01:20 -0400

    Adds Gitpod (#1)

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,15 @@ import adapter from '@sveltejs/adapter-auto'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    vite: {
+      server: {
+        hmr: {
+          clientPort: process.env.HMR_HOST ? 443 : 24678,
+          host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
+        }
+      }
+    }
   }
 }
 
