7150258b01735703def74fdda94224b7c92c84ac
Josh Nussbaum <joshnuss@gmail.com>
2022-06-07 22:18:02 -0400

    Fixup hmr

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,20 @@ import adapter from '@sveltejs/adapter-auto'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    vite: {
+      server: {
+        // configure vite for HMR with Gitpod
+        hmr: process.env.GITPOD_WORKSPACE_URL
+        ? {
+          // removes the protocol and replaces it with the port we're connecting to
+          host: process.env.GITPOD_WORKSPACE_URL.replace('https://', '3000-'),
+          protocol: 'wss',
+          clientPort: 443
+        }
+        : true
+      }
+    }
   }
 }
 
